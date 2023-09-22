435f39f8505d76dcca134bfb130888c41c72b316
MinhAnh-haui <luongminhanh.haui@gmail.com>
2023-09-22 22:56:27 +0700

    display products

--- views/home.php
+++ views/home.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=, initial-scale=1.0">
-    <title>Star book </title>
-</head>
-<body>
-    home
-</body>
-</html>
+<?php
+$query = "select * from products where status = 1";
+$result = $connect->query($query);
+?>
+<section class="products">
+    <?php foreach ($result as $item) : ?>
+        <section class="product">
+            <section class="pro_img"><img src="images/<?= $item['image'] ?>"></section>
+            <section class="pro_name"><?= $item['name'] ?></section>
+            <section class="pro_name"><?= number_format($item['price'],0,',','.') ?>đ</section>
+            <section class="pro_name"><input type="submit" value="Đặt mua"></section>
+        </section>
+    <?php endforeach; ?>
+</section>
