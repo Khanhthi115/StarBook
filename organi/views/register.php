07e9cc2e619cea336197da22e272a5e11dd9da8c
MinhAnh-haui <luongminhanh.haui@gmail.com>
2023-11-24 13:00:20 +0700

    add validate infor registration

--- organi/views/register.php
+++ organi/views/register.php
@@ -1,21 +1,31 @@
 <?php
-if (isset($_POST['username'])) {
+if (isset($_POST['signup'])) {
     $username = $_POST['username'];
-    $query = "select * from member where username = '$username'";
-    $result = $connect->query($query);
-    if (mysqli_num_rows($result) != 0) {
-        $alert = "Tên đăng nhập đã tồn tại";
+    $password = $_POST['password'];
+    $repassword = $_POST['repassword'];
+    $fullname = $_POST['fullname'];
+    $phone = $_POST['phone'];
+    $address = $_POST['address'];
+    $email = $_POST['email'];
+
+    if (empty($username) || empty($password) || empty($repassword) || empty($fullname) || empty($phone) || empty($address) || empty($email)) {
+        echo "<script>alert('Vui lòng điền đầy đủ thông tin')</script>";
+    } elseif ($password !== $repassword) {
+        echo "<script>alert('Mật khẩu nhập lại không khớp')</script>";
+    } elseif (!preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/', $password)) {
+        echo "<script>alert('Mật khẩu phải gồm ít nhất 6 ký tự, bao gồm chữ và số')</script>";
     } else {
-        $password = md5($_POST['password']);
-        $fullname = $_POST['fullname'];
-        $phone = $_POST['phone'];
-        $address = $_POST['address'];
-        $email = $_POST['email'];
-        $query = "insert `member` (
-                `username`, `password`, `fullname`, `phonenumber`, `address`, `email`) values ('$username', '$password',
-                '$fullname', '$phone', '$address', '$email')";
-        $connect->query($query);
-        echo "<script>alert('Đăng ký thành công'); location='?option=home'</script>";
+        // Kiểm tra username đã tồn tại trong database
+        $query = "SELECT * FROM member WHERE username = '$username'";
+        $result = $connect->query($query);
+        if (mysqli_num_rows($result) != 0) {
+            echo "<script>alert('Tên đăng nhập đã tồn tại')</script>";
+        } else {
+            $password = md5($password);
+            $query = "INSERT INTO member (username, password, fullname, phonenumber, address, email) VALUES ('$username', '$password', '$fullname', '$phone', '$address', '$email')";
+            $connect->query($query);
+            echo "<script>alert('Đăng ký thành công'); location='?option=home'</script>";
+        }
     }
 }
 ?>
@@ -36,14 +46,14 @@ if (isset($_POST['username'])) {
             <div class="signup-content" style="animation-name: fadeIn; animation-duration: 2s">
                 <div class="signup-form">
                     <h2 class="form-title">Đăng ký tài khoản</h2>
-                    <form method="POST" class="register-form" id="register-form" onsubmit="if(repassword.value!=password.value){alert('Mật khẩu nhập lại không khớp'); return false;}">
+                    <form method="POST" class="register-form" id="register-form">
                         <div class="form-group">
                             <label for="username"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                            <input type="text" name="username" id="username" placeholder="Tên đăng nhập" />
+                            <input type="text" name="username" id="username" placeholder="Tên đăng nhập" value="<?= isset($username) ? $username : '' ?>" />
                         </div>
                         <div class="form-group">
                             <label for="email"><i class="zmdi zmdi-email"></i></label>
-                            <input type="email" name="email" id="email" placeholder="Email" />
+                            <input type="email" name="email" id="email" placeholder="Email" value="<?= isset($email) ? $email : '' ?>" />
                         </div>
                         <div class="form-group">
                             <label for="pass"><i class="zmdi zmdi-lock"></i></label>
@@ -55,15 +65,15 @@ if (isset($_POST['username'])) {
                         </div>
                         <div class="form-group">
                             <label for="re-fullname"><i class="zmdi zmdi-lock-outline"></i></label>
-                            <input type="text" name="fullname" id="fullname" placeholder="Tên đầy đủ" />
+                            <input type="text" name="fullname" id="fullname" placeholder="Tên đầy đủ" value="<?= isset($fullname) ? $fullname : '' ?>" />
                         </div>
                         <div class="form-group">
                             <label for="phone"><i class="zmdi zmdi-lock-outline"></i></label>
-                            <input type="tel" name="phone" id="phone" placeholder="Số điện thoại" />
+                            <input type="tel" name="phone" id="phone" placeholder="Số điện thoại" value="<?= isset($phone) ? $phone : '' ?>" />
                         </div>
                         <div class="form-group">
                             <label for="address"><i class="zmdi zmdi-lock-outline"></i></label>
-                            <input type="text" name="address" id="address" placeholder="Địa chỉ" />
+                            <input type="text" name="address" id="address" placeholder="Địa chỉ" value="<?= isset($address) ? $address : '' ?>" />
                         </div>
                         <div class="form-group">
                             <input type="submit" name="signup" id="signup" class="btn btn-primary btn-signup" value="Đăng ký" />
