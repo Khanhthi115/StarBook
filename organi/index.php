e1540681ff7ca9b6327092d4e86989456fb0b70f
DaoThiDiem <daothidiem84@gmail.com>
2023-12-14 11:35:15 +0700

    add products

--- organi/index.php
+++ organi/index.php
@@ -1,5 +1,5 @@
 <?php
-$connect = new MySQLi('localhost', 'root', '', 'starbook_databse',3310);
+$connect = new MySQLi('localhost', 'root', '', 'starbook_databse',3306);
 const min_money = 200000;
 const shipping_fee = 30000;
 ?>
